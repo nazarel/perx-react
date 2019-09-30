04ebba5f3f2b0425c58dc9354e308b7717a0353d
Tone Kohji <tone.kohji@gmail.com>
2019-09-30 12:37:04 +0800

    Updates

---
+++ precache-manifest.b242aefddbe6201c9a92ddba9438c2a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f182efe5184cc4d32c2e304f233570d",
+    "url": "/Perx-React/index.html"
+  },
+  {
+    "revision": "e6d33a9b9bbb6b516148",
+    "url": "/Perx-React/static/css/main.d95f6789.chunk.css"
+  },
+  {
+    "revision": "0c7cb1011d2be816d8c1",
+    "url": "/Perx-React/static/js/2.2f0374cf.chunk.js"
+  },
+  {
+    "revision": "e6d33a9b9bbb6b516148",
+    "url": "/Perx-React/static/js/main.d0a57ace.chunk.js"
+  },
+  {
+    "revision": "9b0ef9f3715dfa922867",
+    "url": "/Perx-React/static/js/runtime-main.47fb55c6.js"
+  }
+]);
