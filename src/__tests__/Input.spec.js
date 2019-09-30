5acab21c8a67a504e5d8fefbd6df1a83ae037e08
Tone Kohji <tone.kohji@gmail.com>
2019-09-30 21:03:00 +0800

    - minot revision for test cases

--- src/__tests__/Input.spec.js
+++ src/__tests__/Input.spec.js
@@ -1,14 +1,27 @@
 import renderer from 'react-test-renderer';
 import Input from '../components/Input';
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import enzymeConfig from '../../enzymeConfig';
-import { Provider } from "react-redux";
-import store from "../store";
+import * as mock_dummyData from './dummy/data';
+jest.mock('react-redux', () => ({
+  useDispatch: ()=> { },
+  useSelector: () => ({
+    OrganizationReducer: {
+      isError: true,
+      error: mock_dummyData.error
+    },
+    RepoReducer: {
+      isError: true,
+      error: mock_dummyData.error
+    }
+  })
+}));
 
 describe('<Input/>', function() {
+
   it('Should render correctly', function(){
-    const component = renderer.create(<Provider store={store}><Input /></Provider>);
+    const component = renderer.create(<Input />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
@@ -16,18 +29,26 @@ describe('<Input/>', function() {
 
   it('Should have correct label and input type', function(){
 
-    const component = mount(<Provider store={store}><Input label={'Number'} type={'number'}/></Provider>);
+    const component = shallow(<Input label={'Number'} type={'number'}/>);
     expect(component.find('span').at(0).text()).toEqual('Number');
     expect(component.find('input.form-control[type="number"]')).toHaveLength(1);
   });
 
   it('Text input should be correct', function() {
 
-    const component = mount(<Provider store={store}><Input label={'Test'} type={'text'}/></Provider>);
+    const component = mount(<Input label={'Test'} type={'text'}/>);
     const input = component.find('input.form-control');
     input.simulate('change', {target: {value: 'someth ing'}});
     expect(input.instance().value).toEqual('something');
 
+  })
+
+  it('Error messages should be correct', function() {
+    const component = shallow(<Input/>);
+
+    component.find('button').simulate('click');
+    const errorMessage = component.find('span.text-danger');
+    expect(errorMessage.text()).toEqual('*You need to enter a username to search')
   })
 
 //TODO:  Add more tests related to error display & data fetching test using nock and async-wait-until
